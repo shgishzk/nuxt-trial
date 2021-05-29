90273a13494d85dafff8db7a1152922c4ec2c6cb
Shogo Ishizaka <ishizaka@lep.jp>
2021-05-29 15:38:15 +0700

    feat: Display sample chart

---
+++ components/ChartCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-card>
+    <v-card-title>
+      Pressure Chart
+    </v-card-title>
+
+    <v-card-text class="chart-container">
+      <ChartCardStore :chartData="chartData" :options="options"/>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import ChartCardStore from './LineChart.vue';
+export default {
+    components: {
+        ChartCardStore
+    },
+    data() {
+        return {
+            chartData: {
+                labels: this.$store.state.chart.pressures.label,
+                datasets: [{
+                    label: "Pressure",
+                    data: this.$store.state.chart.pressures.data,
+                    fill: true,
+                    backgroundColor: 'rgba(46, 170, 232, 0.4)',
+                    borderColor: '#ABE1FA'
+                }]
+            },
+
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                legend: {
+                    display: true
+                },
+            }
+        }
+    },
+}
+</script>
